805fea6f1f0bb06c684c866dc08bb9bb1efe84a9
giuxtaposition <yg97.cs@gmail.com>
2021-06-03 13:56:42 +0200

    added simple footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "../styles/Footer.scss";
 
 const Footer: React.FC = () => {
-  return <section className="Footer">My Footer</section>;
+  return (
+    <section className="Footer">
+      <a
+        className="copyright"
+        href="https://github.com/giuxtaposition/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        © Designed and Built by Giuxtaposition (Giulia Ye)
+      </a>
+    </section>
+  );
 };
 
 export default Footer;
